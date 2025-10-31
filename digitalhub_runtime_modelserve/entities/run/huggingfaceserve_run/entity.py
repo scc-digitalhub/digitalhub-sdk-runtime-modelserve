b9bd8fb38efe9492326557744c454a7d198569cb
EvangMM <matteomartini92@yahoo.it>
2025-10-31 09:47:07 +0100

    chore: fix invoke

--- digitalhub_runtime_modelserve/entities/run/huggingfaceserve_run/entity.py
+++ digitalhub_runtime_modelserve/entities/run/huggingfaceserve_run/entity.py
@@ -71,7 +71,7 @@ class RunHuggingfaceserveRun(RunModelserveRun):
                 " If a service is deploying, use run.wait() or try again later."
             )
 
-        if url is not None and not url.startswith(base_url.removeprefix("http://").removeprefix("https://")):
+        if url is not None and not if url is not None and not url.removeprefix("http://").removeprefix("https://").startswith(base_url)::
             raise EntityError(f"Invalid URL: {url}. It must start with the service URL: {base_url}")
 
         if url is None:

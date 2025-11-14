91d20e6c96b4230732d9c38f4915d495311f754e
EvangMM <matteomartini92@yahoo.it>
2025-11-14 15:04:15 +0100

    chore: moved to 0.15b

--- digitalhub_runtime_modelserve/entities/task/huggingfaceserve_serve/spec.py
+++ digitalhub_runtime_modelserve/entities/task/huggingfaceserve_serve/spec.py
@@ -19,16 +19,11 @@ class TaskSpecHuggingfaceserveServe(TaskSpecModelserveServe):
     def __init__(
         self,
         function: str,
-        node_selector: list[dict] | None = None,
         volumes: list[dict] | None = None,
         resources: dict | None = None,
-        affinity: dict | None = None,
-        tolerations: list[dict] | None = None,
         envs: list[dict] | None = None,
         secrets: list[str] | None = None,
         profile: str | None = None,
-        runtime_class: str | None = None,
-        priority_class: str | None = None,
         huggingface_task: str | None = None,
         backend: str | None = None,
         tokenizer_revision: str | None = None,
@@ -44,19 +39,13 @@ class TaskSpecHuggingfaceserveServe(TaskSpecModelserveServe):
     ) -> None:
         super().__init__(
             function,
-            node_selector,
             volumes,
             resources,
-            affinity,
-            tolerations,
             envs,
             secrets,
             profile,
-            runtime_class,
-            priority_class,
             **kwargs,
         )
-
         self.huggingface_task = huggingface_task
         self.backend = backend
         self.tokenizer_revision = tokenizer_revision
--- digitalhub_runtime_modelserve/entities/task/modelserve_serve/spec.py
+++ digitalhub_runtime_modelserve/entities/task/modelserve_serve/spec.py
@@ -17,16 +17,11 @@ class TaskSpecModelserveServe(TaskSpecFunction):
     def __init__(
         self,
         function: str,
-        node_selector: list[dict] | None = None,
         volumes: list[dict] | None = None,
         resources: dict | None = None,
-        affinity: dict | None = None,
-        tolerations: list[dict] | None = None,
         envs: list[dict] | None = None,
         secrets: list[str] | None = None,
         profile: str | None = None,
-        runtime_class: str | None = None,
-        priority_class: str | None = None,
         replicas: int | None = None,
         service_type: str | None = None,
         service_name: str | None = None,
@@ -34,16 +29,11 @@ class TaskSpecModelserveServe(TaskSpecFunction):
     ) -> None:
         super().__init__(
             function,
-            node_selector,
             volumes,
             resources,
-            affinity,
-            tolerations,
             envs,
             secrets,
             profile,
-            runtime_class,
-            priority_class,
             **kwargs,
         )
         self.replicas = replicas
